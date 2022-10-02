de99804a36068984f17ac11488669fc9e1f57947
richie48 <odiorrichard@gmail.com>
2022-10-02 12:32:48 +0100

    worked on countdown logic

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 const countdown =()=>{
-    const date=new Date("May 30, 2021 00:00:00")
+    const endDate=new Date("January 30, 2023 00:00:00").getTime()
+
+    const now= new Date().getTime()
+    const difference=endDate-now
+
+    //second, minutes, hours, days constant
+    let seconds=1000
+    let minutes=seconds*60
+    let hours=minutes*60
+    let days=hours*24
+
+    let timeDays=Math.floor(difference/days)
+    let timeHours=Math.floor(difference%days/hours)
+    let timeMinutes=Math.floor(difference%hours/minutes)
+    let timeSeconds=Math.floor(difference%minutes/seconds)
+
+    timeHours=timeHours<10?"0"+timeHours: timeHours
+    timeMinutes=timeMinutes<10?"0"+timeMinutes: timeMinutes
+    timeSeconds=timeSeconds<10?"0"+timeSeconds: timeSeconds
+
+
+    document.getElementById("days").innerHTML=timeDays
+    document.getElementById("hours").innerHTML=timeHours
+    document.getElementById("minutes").innerHTML=timeMinutes
+    document.getElementById("seconds").innerHTML=timeSeconds
 }
 
-countdown()
+setInterval(countdown,1000)
